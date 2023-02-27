6c93ef201061bcc10252d36eff4b9c904cbc78b0
apades <apad2@qq.com>
2023-02-27 10:07:29 +0800

    init: vite3 + vue2

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="app">
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld :msg="msg" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      msg: 'hello view'
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+.app{
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  place-items: center;
+  justify-content: center;
+  align-items: center;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
